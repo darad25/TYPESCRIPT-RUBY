a9001b74834f0867631f2161314bc78f4dc0d8a6
1amdara <77836449+1amdara@users.noreply.github.com>
2023-12-30 12:31:40 -0800

    first commit

---
+++ app.rb
@@ -0,0 +1,49 @@
+# Assume a function to get a random number between 100 and 200
+def rand()
+    rand(100..200)
+  end
+  
+  # Constants
+  tubes_per_unit = 4
+  units_in_classroom = 4
+  hours_per_day = 15
+  days_per_week = 5
+  weeks_per_year = 39
+  months_per_year = 9
+  cost_per_tube = 7
+  
+  # Function to calculate the total broken tubes and cost
+  def calculate_cost()
+    total_tubes = 0
+    total_cost = 0
+  
+    (1..units_in_classroom).each do |unit|
+      tubes_in_unit = tubes_per_unit
+      hours_left = 0
+  
+      (1..days_per_week * weeks_per_year * months_per_year).each do |day|
+        (1..tubes_in_unit).each do |_tube|
+          if hours_left <= 0
+            hours_left = rand()
+          else
+            hours_left -= 1
+          end
+        end
+  
+        # If 2 tubes failed, replace all 4 tubes in the unit
+        if tubes_in_unit <= tubes_per_unit - 2
+          total_tubes += tubes_in_unit
+          tubes_in_unit = tubes_per_unit
+        end
+      end
+    end
+  
+    total_cost = total_tubes * cost_per_tube
+    return { total_tubes: total_tubes, total_cost: total_cost }
+  end
+  
+  # Calculate and print the result
+  result = calculate_cost()
+  puts "Total broken tubes in 1 year: #{result[:total_tubes]}"
+  puts "Total cost per year: $#{result[:total_cost]}"
+  
---
+++ app.tsx
@@ -0,0 +1,50 @@
+// Assume a function to get a random number between 100 and 200
+function rand(): number {
+    return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+  }
+  
+  // Constants
+  const tubesPerUnit = 4;
+  const unitsInClassroom = 4;
+  const hoursPerDay = 15;
+  const daysPerWeek = 5;
+  const weeksPerYear = 39;
+  const monthsPerYear = 9;
+  const costPerTube = 7;
+  
+  // Function to calculate the total broken tubes and cost
+  function calculateCost(): { totalTubes: number; totalCost: number } {
+    let totalTubes = 0;
+    let totalCost = 0;
+  
+    for (let unit = 1; unit <= unitsInClassroom; unit++) {
+      let tubesInUnit = tubesPerUnit;
+      let tubesFailed = 0;
+      let hoursLeft = 0;
+  
+      for (let day = 1; day <= daysPerWeek * weeksPerYear * monthsPerYear; day++) {
+        for (let tube = 1; tube <= tubesInUnit; tube++) {
+          if (hoursLeft <= 0) { 
+            hoursLeft = rand();
+          } else {
+            hoursLeft--;
+          }
+        }
+  
+        // If 2 tubes failed, replace all 4 tubes in the unit
+        if (tubesFailed <= tubesPerUnit - 2) {
+          totalTubes += tubesInUnit;
+          tubesInUnit = tubesPerUnit;
+        }
+      }
+    }
+  
+    totalCost = totalTubes * costPerTube;
+    return { totalTubes, totalCost };
+  } 
+  
+  // Calculate and print the result
+  const result = calculateCost();
+  console.log(`Total broken tubes in 1 year: ${result.totalTubes}`);
+  console.log(`Total cost per year: $${result.totalCost}`);
+  
